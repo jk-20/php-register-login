fdfaf0763b6740a1e2cd97ca3fc4741cd43c2c6e
jogindrakumar <joginder9868@outlook.com>
2019-09-17 11:35:45 +0530

    addUser.phpAndModifyIndex

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
 <?php include "includes/db.php"; ?>
+<?php session_start(); ?>
+<?php 
 
+$msg = '';
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm-password'];
+
+$username = mysqli_real_escape_string($conn,$username);
+$email = mysqli_real_escape_string($conn,$email);
+$password = mysqli_real_escape_string($conn,$password);
+$confirm_password = mysqli_real_escape_string($conn,$confirm_password);
+$crypt_pass = password_hash($password,PASSWORD_BCRYPT,[10]);
+
+
+if($password === $confirm_password){
+    $sql = "INSERT INTO users(username,email,pass,user_role) VALUES('$username','$email','$crypt_pass','visiter')";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+      
+    $msg = " <p class='text-center alert-success'>Registered successfully!!</p> ";
+        
+       
+
+    }else{
+
+        $msg = " <p class='text-center alert-danger'>something wrong !!</p> ";
+
+    }
+}else{
+    $msg = " <p class='text-center alert-danger'>Both password should be same</p> ";
+}
+
+
+}
+
+
+
+
+?>
 
 
 <!DOCTYPE html>
@@ -11,21 +52,30 @@
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Noto+Serif&display=swap" rel="stylesheet">
-    <title>blog post</title>
+    <title>Register || Login</title>
 
     <style>
     
     body{
-   ; /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: #b92b27;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #1565C0, #b92b27);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #1565C0, #b92b27); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 font-family: 'Noto Serif', serif;
     }
+    .btn{
+        background: #b92b27;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #1565C0, #b92b27);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #1565C0, #b92b27);
+    }
    
-   
-    
+    .navbar {
+        background: #b92b27;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #1565C0, #b92b27);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #1565C0, #b92b27);
+    }
     </style>
 </head>
 <body>
-
 <div class="container">
 <div class="row">
 <div class="col-sm-12">
@@ -37,13 +87,13 @@ font-family: 'Noto Serif', serif;
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
+        <a class="nav-link text-white" href="login.php">Login</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="register.php">Sign up</a>
+        <a class="nav-link text-white" href="register.php">Sign up</a>
       </li>
     
     </ul>
@@ -57,26 +107,53 @@ font-family: 'Noto Serif', serif;
 
 </div>
 
+<p class='text-center text-danger'></p>
+</div>
+<br>
+<div class="container ">
+<div class="row justify-content-center">
+<div class="col-sm-4 border  bg-white">
+<h4 class="text-center text-primary">CREATE ACCOUNT</h4>
 
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+<?php echo $msg; ?>
+<div class="form-group">
+<label for="name">User Name</label>
+<input type="text" class="form-control" name="username">
+
+</div class="style">
+<div class="form-group">
+<label for="email">E-mail</label>
+<input type="email" class="form-control" name="email">
+
+</div>
+<div class="form-group">
+<label for="password">Password</label>
+<input type="password" class="form-control" name="password">
+
+</div>
+<div class="form-group">
+<label for="confirm-password">Confirm-password</label>
+<input type="password" class="form-control" name="confirm-password">
+
+</div>
+<div class="form-group">
+
+<input type="submit" class="form-control btn text-white" name="submit" value="REGISTER">
+
+</div>
+
+</form>
+
+<p>Have already an account ? <a href="login.php">Login Here</a> </p>
 </div>
 
 
 
+</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</div>
+</div>
 
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- login.php
+++ login.php
@@ -1,4 +1,37 @@
 <?php include "includes/db.php"; ?>
+<?php session_start(); ?>
+<?php 
+if(isset($_POST['submit'])){
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $username = mysqli_real_escape_string($conn,$username);
+  $password = mysqli_real_escape_string($conn,$password);
+
+$query = "SELECT * FROM users WHERE username = '$username'";
+$result = mysqli_query($conn,$query);
+if(!$result){
+  die("query failed".mysqli_error($conn));
+}
+while($row=mysqli_fetch_array($result)){
+
+$user_id= $row['id'];
+$db_username = $row['username'];
+$db_password = $row['pass'];
+
+}
+if(password_verify($password,$db_password)){
+  $_SESSION['username'] = $db_username;
+  $_SESSION['pass'] = $db_password;
+  header("Location: user.php");
+}
+
+}
+
+
+?>
+
 
 
 
@@ -45,9 +78,7 @@ background: linear-gradient(to right, #1565C0, #b92b27);
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
+      
       <li class="nav-item">
         <a class="nav-link text-white" href="login.php">Login</a>
       </li>
@@ -74,11 +105,11 @@ background: linear-gradient(to right, #1565C0, #b92b27);
 <div class="col-sm-4 border  bg-white">
 <h4 class="text-center text-primary">Login</h4>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 
 <div class="form-group">
 <label for="name">User Name</label>
-<input type="text" class="form-control" name="name">
+<input type="text" class="form-control" name="username">
 
 <div class="form-group">
 <label for="password">Password</label>
@@ -93,7 +124,7 @@ background: linear-gradient(to right, #1565C0, #b92b27);
 </div>
 
 </form>
-<p>Don't have an account ? <a href="index.php">Sign Up here</a> </p>
+<p>Don't have an account ? <a href="register.php">Sign Up here</a> </p>
 
 </div>
 
